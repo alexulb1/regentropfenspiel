8fbdaafab60c01a683266ab98fc8ed78e1ab9423
alexulb1 <109351274+alexulb1@users.noreply.github.com>
2024-10-08 09:25:50 +0200

    Geschwindigkeit der Wassertropfen erhöht sich mit höherem Score in 5er Schritten

--- main.ts
+++ main.ts
@@ -15,14 +15,16 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     countdown()
     basic.pause(500)
+    wasser = game.createSprite(randint(0, 4), 0)
+    eimer = game.createSprite(2, 4)
     wasser.turn(Direction.Right, 90)
     status = 1
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     eimer.move(1)
 })
-let eimer: game.LedSprite = null
 let wasser: game.LedSprite = null
+let eimer: game.LedSprite = null
 let status = 0
 let counter = 0
 counter = 3
@@ -30,16 +32,24 @@ status = 0
 basic.clearScreen()
 game.setLife(5)
 game.setScore(0)
-wasser = game.createSprite(0, 0)
-eimer = game.createSprite(2, 4)
 basic.forever(function () {
     if (status == 1) {
-        basic.pause(200)
+        if (game.score() < 5) {
+            basic.pause(500)
+        } else if (game.score() < 10) {
+            basic.pause(400)
+        } else if (game.score() < 15) {
+            basic.pause(300)
+        } else if (game.score() < 20) {
+            basic.pause(200)
+        } else {
+            basic.pause(100)
+        }
         if (wasser.get(LedSpriteProperty.Y) < 4) {
             wasser.move(1)
         } else {
             if (wasser.isTouching(eimer)) {
-                basic.setLedColor(0xff0000)
+                game.addScore(1)
             } else {
                 game.removeLife(1)
             }
